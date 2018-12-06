af7eaef6dd02a8fe620e88fdd62c14ece4809311
KevinNutellaBReady <kevin26032006@gmail.com>
2018-12-06 19:58:07 +0100

    Fix

--- src/me/kevin/listener/Joinlistener.java
+++ src/me/kevin/listener/Joinlistener.java
@@ -6,6 +6,7 @@
 package me.kevin.listener;
 
 import me.kevin.managers.ItemManager;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -18,12 +19,17 @@ import org.bukkit.event.player.PlayerJoinEvent;
  * @author haral
  */
 public class Joinlistener implements Listener {
+   
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
         e.setJoinMessage(null);
         Player p = e.getPlayer();
+        p.setGameMode(GameMode.SURVIVAL);
+        p.setHealth(20);
+        p.setMaxHealth(20);
+        p.setFoodLevel(7);
         
-                p.getInventory().setItem(0, new ItemManager("§bSchwert", Material.GOLD_SWORD, (byte)0, 1, Enchantment.DURABILITY).build());
+        p.getInventory().setItem(0, new ItemManager("§bSchwert", Material.GOLD_SWORD, (byte)0, 1, Enchantment.DURABILITY).build());
         p.getInventory().setItem(2, new ItemManager("§bAngel", Material.FISHING_ROD, (byte)0, 1, "").build());
         p.getInventory().setItem(1, new ItemManager("§bKnüppel", Material.STICK, (byte)0, 1, Enchantment.ARROW_KNOCKBACK).build());
         p.getInventory().setItem(3, new ItemManager("§bEnderpearl", Material.ENDER_PEARL, (byte)0, 1, "").build());
@@ -33,7 +39,9 @@ public class Joinlistener implements Listener {
         p.getInventory().setItem(7, new ItemManager("§bBlöcke", Material.SANDSTONE, (byte)0, 64, "").build());
         p.getInventory().setItem(8, new ItemManager("§bBlöcke", Material.SANDSTONE, (byte)0, 64, "").build());
         p.chat("/warp spawn");
+        p.chat("/warp spawn");
+        
         
     }
-    
+   
 }
